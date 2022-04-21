1c2180288279a99c63ad64d92e0a32427f1dfb03
Dave van der Meer <dave.vandermeer@uni.lu>
2022-04-21 16:21:39 +0200

    Clean workspace.

--- _host_setup/setup.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-HOST_SETUP_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
-
-# ROS 2
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file://${HOST_SETUP_DIR}/cyclonedds.xml
-export ROS_DOMAIN_ID=42
-export ROS_LOCALHOST_ONLY=0
