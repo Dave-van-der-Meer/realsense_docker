f8546799fdb1d216f296703caf64645e910faf16
Dave van der Meer <dave.vandermeer@uni.lu>
2024-05-13 16:26:55 +0200

    Add convenience scripts

---
+++ docker/docker_test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+if [ ${#} -lt 1 ]; then
+    #echo "Usage: ${0} <docker image> <cmd (optional)>"
+    #exit 1
+    IMG="local/realsense:humble"
+    CMD="bash"
+fi
+
+ENVS="--env ROS_NAMESPACE=${ROS_NAMESPACE}"
+if [ -n "${ROS_DOMAIN_ID}" ]; then
+    ENVS="${ENVS} --env ROS_DOMAIN_ID=${ROS_DOMAIN_ID}"
+fi
+if [ -n "${ROS_LOCALHOST_ONLY}" ]; then
+    ENVS="${ENVS} --env ROS_LOCALHOST_ONLY=${ROS_LOCALHOST_ONLY}"
+fi
+
+DOCKER_RUN_CMD=(
+    docker run
+    --interactive
+    --tty
+    --name "leo_realsense"
+    --network host
+    --ipc host
+    --rm
+    --privileged
+    --security-opt "seccomp=unconfined"
+    --volume "/etc/localtime:/etc/localtime:ro"
+    --volume "/dev:/dev"
+    --volume "${PWD}/../ros2_ws/src/leorover_realsense:/home/leo/ros2_ws/src/leorover_realsense"
+    --env ROS_NAMESPACE=${ROS_NAMESPACE}
+    --env ROS_DOMAIN_ID=${ROS_DOMAIN_ID}
+    --env ROS_LOCALHOST_ONLY=${ROS_LOCALHOST_ONLY}
+    "${ENVS}"
+    "${IMG}"
+    "${CMD}"
+)
+
+echo -e "\033[1;30m${DOCKER_RUN_CMD[*]}\033[0m" | xargs
+
+# shellcheck disable=SC2048
+exec ${DOCKER_RUN_CMD[*]}
